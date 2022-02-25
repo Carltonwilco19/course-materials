14ef480652ddab26339167249ec98e87c4b00849
cwilcox4 <cwilcox4@uwyo.edu>
2022-02-24 23:48:52 -0500

    Completed Lab 3b

--- materials/lab/3/main/main.go
+++ materials/lab/3/main/main.go
@@ -1,7 +1,7 @@
 // Build and Use this File to interact with the shodan package
 // In this directory lab/3/shodan/main:
 // go build main.go
-// SHODAN_API_KEY=YOURAPIKEYHERE ./main <search term>
+// SHODAN_API_KEY=B0WFcVTmIGluwigF6ys0un4jnA1Vqsab ./main <search term> <pagenumber>
 
 package main
 
@@ -9,13 +9,14 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"encoding/json"
+//	"encoding/json"
 	"shodan/shodan"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalln("Usage: main <searchterm>")
+	if len(os.Args) != 3  {
+		log.Fatalln("Usage: main <searchterm> <startingpage>") // main is the first argument and the serach term is the second
 	}
 	apiKey := os.Getenv("SHODAN_API_KEY")
 	s := shodan.New(apiKey)
@@ -23,31 +24,36 @@ func main() {
 	if err != nil {
 		log.Panicln(err)
 	}
+
 	fmt.Printf(
 		"Query Credits: %d\nScan Credits:  %d\n\n",
 		info.QueryCredits,
 		info.ScanCredits)
 
-	hostSearch, err := s.HostSearch(os.Args[1])
+	i,_ := strconv.Atoi(os.Args[2]) // parse string arg to int
+	hostSearch, err := s.HostSearch(os.Args[1], i)// here I would add in the page number, extending main.go
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	fmt.Printf("Host Data Dump\n")
+	// fmt.Printf("Host Data Dump\n")
+	// for _, host := range hostSearch.Matches {
+	// 	fmt.Println("==== start ",host.IPString,"====")
+	// 	h,_ := json.Marshal(host)
+	// 	fmt.Println(string(h))
+	// 	fmt.Println("==== end ",host.IPString,"====")
+	// 	//fmt.Println("Press the Enter Key to continue.")
+	// 	//fmt.Scanln()
+	// }
+
+	
+
+
+
+	fmt.Printf("IP, Port, City\n")
+
 	for _, host := range hostSearch.Matches {
-		fmt.Println("==== start ",host.IPString,"====")
-		h,_ := json.Marshal(host)
-		fmt.Println(string(h))
-		fmt.Println("==== end ",host.IPString,"====")
-		//fmt.Println("Press the Enter Key to continue.")
-		//fmt.Scanln()
-	}
-
-
-	fmt.Printf("IP, Port\n")
-
-	for _, host := range hostSearch.Matches {
-		fmt.Printf("%s, %d\n", host.IPString, host.Port)
+		fmt.Printf("IP: %s, Port: %d, City: %s\n", host.IPString, host.Port, host.Location.City)
 	}
 
 
